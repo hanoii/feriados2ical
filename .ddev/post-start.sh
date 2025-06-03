a8fbd652851bc50d51dbae7b78be6d6859c1ab1a
Ariel Barreiro <abarrei@gmail.com>
2025-06-03 11:35:32 -0300

    build: requirements.txt

--- .ddev/post-start.sh
+++ .ddev/post-start.sh
@@ -3,4 +3,4 @@
 [ ! -d .venv ] && python3 -m venv .venv
 [ -d .venv ] && python3 -m venv --upgrade .venv
 source .venv/bin/activate
-# pip install -r requirements.txt
+pip install -r requirements.txt
